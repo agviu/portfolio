0189acfaa8b5096bdbfff84a4f7d0a1fd595d855
Alberto G. Viu <albertogviu@gmail.com>
2023-12-16 23:44:17 +0100

    Implemented getLatestsDate function

--- lib/utils/dates.dart
+++ lib/utils/dates.dart
@@ -13,24 +13,44 @@ class DateUtils {
   /// [from] a formatted String that can be used by DateTime.parse
   static dynamic getLastDate(
       {TimeMode mode = TimeMode.yearWeek, String from = 'now'}) {
-    final DateTime date;
-    if (from == 'now') {
-      date = DateTime.now();
-    } else {
-      date = DateTime.parse(from);
+    return getLatestsDates(1, mode: mode, from: from).removeLast();
+  }
+
+  /// Returns the latest [numberOfTimes] dates, depending on the time_mode passed.
+  ///
+  /// For example, if mode is yearWeek, it will return the lastest [numberOfTimes] weeks
+  /// that has happened previous to the value passed in from.
+  ///
+  /// [numberOfTimes] is the number of times to return in the list. E.g. number of weeks.
+  /// [mode] is the mode to use for getting the last date.
+  /// [from] a formatted String that can be used by DateTime.parse
+  static List<dynamic> getLatestsDates(int numberOfTimes,
+      {TimeMode mode = TimeMode.yearWeek, String from = 'now'}) {
+    if (numberOfTimes < 1) {
+      throw ArgumentError('numberOfTimes must be at least 1');
     }
 
-    // Calculates how many days have passed since the begining of the year.
-    final DateTime firstDayOfYear = DateTime(date.year, 1, 1);
-    final int dayOfYear = date.difference(firstDayOfYear).inDays;
+    final DateTime fromDate =
+        (from == 'now') ? DateTime.now() : DateTime.parse(from);
+
+    /// This function calculates and returns, for [date], the number of the week in the
+    /// year that the [date] belongs.
+    int getWeekOfYear(DateTime date) {
+      // Calculates how many days have passed since the begining of the year.
+      final DateTime firstDayOfYear = DateTime(date.year, 1, 1);
+      final int dayOfYear = date.difference(firstDayOfYear).inDays;
+      final int weekOfYear = (dayOfYear / 7).floor() + 1;
+      return weekOfYear;
+    }
 
     if (mode == TimeMode.yearWeek) {
-      final int weeksSinceStartOfYear = (dayOfYear / 7).floor() + 1;
-      List<String> lastDate = [
-        date.year.toString(),
-        weeksSinceStartOfYear.toString()
-      ];
-      return lastDate.join('.');
+      List<String> weeks = [];
+      for (int i = 0; i < numberOfTimes; i++) {
+        DateTime targetDate = fromDate.subtract(Duration(days: 7 * i));
+        int weekOfYear = getWeekOfYear(targetDate);
+        weeks.add('${targetDate.year}.$weekOfYear');
+      }
+      return weeks;
     } else {
       throw UnsupportedError("Only weekYear is supported for now.");
     }
--- test/utils/dates_test.dart
+++ test/utils/dates_test.dart
@@ -1,35 +1,86 @@
 import 'package:test/test.dart';
 import 'package:portfolio/utils/dates.dart';
-import 'package:portfolio/models/time_mode.dart';
+// import 'package:portfolio/models/time_mode.dart';
 
 void main() {
-  const TimeMode mode = TimeMode.yearWeek;
+  // const TimeMode mode = TimeMode.yearWeek;
 
-  test('Gets the latest values', () {
-    // First week of the year:
-    var date = DateTime(2024, 1, 1);
-    var dateString = date.toString();
-    var lastDate = DateUtils.getLastDate(from: dateString);
-    if (lastDate != '2024.1') {
-      fail("For the date 2024-1-1 we expect 2024.1, but received $lastDate");
-    }
+  test(
+    'Gets the latest values',
+    () {
+      // First week of the year:
+      var date = DateTime(2024, 1, 1);
+      var dateString = date.toString();
+      var lastDate = DateUtils.getLastDate(from: dateString);
+      if (lastDate != '2024.1') {
+        fail("For the date 2024-1-1 we expect 2024.1, but received $lastDate");
+      }
 
-    // Fourth week of the year:
-    date = DateTime(2024, 1, 21);
-    dateString = date.toString();
-    lastDate = DateUtils.getLastDate(from: dateString);
-    if (lastDate != '2024.3') {
-      fail("For the date 2024-1-21 we expect 2024.3, but received $lastDate");
-    }
+      // Fourth week of the year:
+      date = DateTime(2024, 1, 21);
+      dateString = date.toString();
+      lastDate = DateUtils.getLastDate(from: dateString);
+      if (lastDate != '2024.3') {
+        fail("For the date 2024-1-21 we expect 2024.3, but received $lastDate");
+      }
 
-    // Check that passing now also works:
-    date = DateTime.now();
-    dateString = date.toString();
-    lastDate = DateUtils.getLastDate(from: dateString);
-    var lastDateNow = DateUtils.getLastDate(from: 'now');
-    if (lastDate != lastDateNow) {
-      fail(
-          "When checking for now, the lastDate was $lastDateNow but we expected $lastDate");
-    }
-  });
+      // Check that passing now also works:
+      date = DateTime.now();
+      dateString = date.toString();
+      lastDate = DateUtils.getLastDate(from: dateString);
+      var lastDateNow = DateUtils.getLastDate(from: 'now');
+      if (lastDate != lastDateNow) {
+        fail(
+            "When checking for now, the lastDate was $lastDateNow but we expected $lastDate");
+      }
+    },
+  );
+
+  test(
+    'Get several last weeks',
+    () {
+      // Get the last 2 weeks from 16/12/2023
+      var date = DateTime(2023, 12, 16);
+      var dateString = date.toString();
+      var weeks = DateUtils.getLatestsDates(2, from: dateString);
+      if (weeks.length != 2) {
+        fail("Expecting 2 weeks, received ${weeks.length}");
+      } else {
+        var firstWeek = weeks[0];
+        var secondWeek = weeks[1];
+        if (firstWeek != '2023.50' && secondWeek != '2023.49') {
+          fail("The values of the last 2 weeks do not match");
+        }
+      }
+
+      // Get the last 6 weeks from 16/2/2023
+      date = DateTime(2023, 1, 16);
+      dateString = date.toString();
+      weeks = DateUtils.getLatestsDates(6, from: dateString);
+      if (weeks.length != 6) {
+        fail("Expecting 6 weeks, received ${weeks.length}");
+      } else {
+        var firstWeek = weeks[0];
+        var fourthWeek = weeks[3];
+        if (firstWeek != '2023.3' && fourthWeek != '2022.52') {
+          fail("The values of 2 weeks do not match");
+        }
+      }
+
+      // Get last 4 months of weeks
+      dateString = DateTime(2024, 3, 23).toString();
+      weeks = DateUtils.getLatestsDates(4 * 4, from: dateString);
+      if (weeks.length != 16) {
+        fail("Expecting 16 weeks, but received ${weeks.length}");
+      }
+      if (weeks[0] != '2024.12' && weeks[15] != '2023.49') {
+        fail("The values of 2 weeks do not match");
+      }
+
+      // Check that the function returns an Exception is number of times is not 1 or bigger number.
+      expect(() {
+        DateUtils.getLatestsDates(0);
+      }, throwsArgumentError);
+    },
+  );
 }
